1eac5394943eeb967717e13d21cd51cccddc6688
AliMys <mohammadali.new123@gmail.com>
2024-11-30 21:35:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('playButton').addEventListener('click', () => {
+    const audio = document.getElementById('audio');
+    audio.play();
+
+    alert("Love you! 💕 Aku sayang kamu sayanggkuu cantikkuu manisskuu muaaah selangit maaf kalo aku hanya bisa ngasih ini :)🫰!");
+});
